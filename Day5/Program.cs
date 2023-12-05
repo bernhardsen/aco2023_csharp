bec489ae2149211c902a9f9eca260d1e7da06863
bernhardsen <bernhardsen@gmail.com>
2023-12-05 20:21:07 +0100

    Had forgotten to remove some timing code.

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -4,10 +4,6 @@ using Day5;
 Console.WriteLine("Advent of Code 2023!");
 Console.WriteLine("Day 5: If You Give A Seed A Fertilizer");
 
-var watch = new System.Diagnostics.Stopwatch();
-
 var solver = new SeedLocator("input.txt");
 solver.SolvePart1();
 solver.SolvePart2();
-Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
-
